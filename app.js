463c52ef5f570f82aef490c28fc82e5411e84c19
Dorian JAMINAIS <dorian@jaminais.fr>
2013-01-11 12:42:04 +0100

    implemted the fibonacci service
    Implemented a service at /fibonacci taking a number in a post request
    and returning the fibonacci of this number.

---
+++ app.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var app = express();
+
+
+app.configure(function(){
+	app.use(express.bodyParser());
+});
+
+/**
+ * Return the fibonacci of num
+ * Fibonacci series is defined by F(n) = F(n-1) + F(n-2); F(0)=0; F(1)=1
+ * @param num index of the number we want in the fibonacci series
+ */
+function fibonacci (num) {
+	console.log(num)
+	switch (num){
+		case 0:
+			return 0;
+			break;
+		case 1:
+			return 1;
+			break;
+		default:
+			return fibonacci(num-1) + fibonacci(num-2);
+			break;
+	}
+}
+
+app.post('/fibonacci', function(req, res){
+	console.log("request fibonacci of "+req.body.number)
+	var number = parseInt(req.body.number);
+	var fibo = fibonacci(number);
+	console.log("resultat : "+fibo);
+	res.send(200, fibo.toString());
+});
+
+app.listen(3000);
+console.log('Listening on port 3000');
