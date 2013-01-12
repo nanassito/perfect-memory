30dbaceafb23831a04bb29aa3ef8c35c8a5a1f4b
Dorian JAMINAIS <dorian@jaminais.fr>
2013-01-12 15:27:47 +0100

    added a linear implementation of the fibonacci serie

--- app.js
+++ app.js
@@ -23,13 +23,22 @@ function fibonacci (num) {
 			return fibonacci(num-1) + fibonacci(num-2);
 			break;
 	}
+}//*/
+
+/**
+ * A linear implementation of the fibonacci series
+ */
+function fibonacci_linear(n){
+	var x = (1+Math.sqrt(5))/2;
+    var fibo = (Math.pow(x,n) - Math.pow(1-x,n))/(x - (1-x));
+    return Math.round(fibo);
 }
 
 
 app.get('/fibonacci', function(req, res){
 	console.log(new Date() + " : request fibonacci of "+req.query["number"])
 	var number = parseInt(req.query["number"]);
-	var fibo = fibonacci(number);
+	var fibo = fibonacci_linear(number);
 	res.send(200, fibo.toString());
 });
 
